5dd15fa540256995a72bed3e37f329bc1333f2ab
Sanne <sannelindholm@hotmail.com>
2020-12-19 23:23:28 +0100

    Removing map functionallity from NewsSearch

--- src/components/NewsSearch.jsx
+++ src/components/NewsSearch.jsx
@@ -8,23 +8,20 @@ const NewsSearch = () => {
 	const dispatch = useDispatch();
 	const articles = useSelector((state) => state.specificNews);
 	const searchNews = async () => {
-    const query = searchValue
+		const query = searchValue;
 		let searchResponse = await NewsService.search(query);
 		dispatch({ type: "SEARCH_NEWS", payload: searchResponse });
 	};
 
-  useEffect(searchNews, []);
-  
-  let searchDisplay = articles.map((article) => {
-    return <NewsCard article={article} />;
-  });
+	useEffect(searchNews, []);
+
 	return (
-    <Input 
-    data-cy="search-input" 
-    action="Search" 
-    placeholder="Search..." 
-    onChange={(event) => searchValue(event)}
-    />
+		<Input
+			data-cy="search-input"
+			action="Search"
+			placeholder="Search..."
+			onChange={(event) => searchValue(event)}
+		/>
 	);
 };
 
