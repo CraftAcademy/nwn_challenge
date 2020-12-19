a71061722720c79cb8ff8d6e80fbacfe93f8c43f
Sanne <sannelindholm@hotmail.com>
2020-12-19 23:06:35 +0100

    Adding searchValue to query variable

--- src/components/NewsSearch.jsx
+++ src/components/NewsSearch.jsx
@@ -7,6 +7,7 @@ const NewsSearch = () => {
 	const dispatch = useDispatch();
 	const articles = useSelector((state) => state.specificNews);
 	const searchNews = async () => {
+    const query = searchValue
 		let searchResponse = await NewsService.search(query);
 		dispatch({ type: "SEARCH_NEWS", payload: searchResponse });
 	};
@@ -17,7 +18,7 @@ const NewsSearch = () => {
     data-cy="search-input" 
     action="Search" 
     placeholder="Search..." 
-    onChange={(event) => performSearch(event)}
+    onChange={(event) => searchValue(event)}
     />
 	);
 };
