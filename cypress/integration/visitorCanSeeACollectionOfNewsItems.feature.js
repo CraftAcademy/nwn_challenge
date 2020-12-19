e24ed0e55c8ccc1ce95269dcf9938cefcc884183
sealfury <willspecker@gmail.com>
2020-12-19 12:11:47 +0100

    Updates test to accomodate further article data Inserts article data into article card

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -16,15 +16,30 @@ describe("Visitor can see a collection of articles", () => {
     it("visitor can see an article\'s title", () => {
       cy.get("[data-cy='article-1']").within(() => {
         cy.get("[data-cy='article-title']").should("contain",
-          "How Do Bitcoin Transactions Work?");
+          "How Do Bitcoin Transactions Work?"
+        );
       });
     })
 
     it("visitor can see an article\'s description", () => {
       cy.get("[data-cy='article-2']").within(() => {
         cy.get("[data-cy='article-desc']").should("contain",
-          "The US dollar is still the primary international global reserve currency and still figures as the most important currency in Forex markets");
+          "The US dollar is still the primary international global reserve currency and still figures as the most important currency in Forex markets"
+        );
       });
+    })
+
+    it("visitor can see an article\'s author and source", () => {
+      cy.get("[data-cy='article-3']").within(() => {
+        cy.get("[data-cy='article-meta']").should("contain", "Lisa Butters");
+        cy.get("[data-cy='article-meta']").should("contain", "Forbes");
+      });
+    })
+
+    it("visitor can see an article\'s publication date", () => {
+      cy.get("[data-cy='article-4']").within(() => {
+        cy.get("[data-cy='article-extra']").should("contain", "Published at 2020-12-10T21:25:19Z")
+      })
     })
   })
 })
