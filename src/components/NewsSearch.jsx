a4d150e8b4c34b456c20e079ecc0d75b4c073dbd
Sanne <sannelindholm@hotmail.com>
2020-12-19 22:54:14 +0100

    Adding useDispatch, useSelector, axios search request from module and useEffect

--- src/components/NewsSearch.jsx
+++ src/components/NewsSearch.jsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import NewsService from '../modules/NewsService'
-import { Input } from 'semantic-ui-react'
-
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import NewsService from "../modules/NewsService";
+import { Input } from "semantic-ui-react";
 
 const NewsSearch = () => {
-  return (
-    <Input data-cy="search-input" action='Search' placeholder='Search...' />
-  )
-}
+	const dispatch = useDispatch();
+	const articles = useSelector((state) => state.specificNews);
+	const searchNews = async () => {
+		let articles = await NewsService.search(query);
+		dispatch({ type: "SEARCH_NEWS", payload: articles });
+	};
 
-export default NewsSearch
+	useEffect(searchNews, []);
+	return (
+		<Input data-cy="search-input" action="Search" placeholder="Search..." />
+	);
+};
+
+export default NewsSearch;
