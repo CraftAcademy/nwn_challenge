1c9a984a3d48be5028914d4a00c221f888b4c88b
Sanne <sannelindholm@hotmail.com>
2020-12-19 22:51:46 +0100

    Adding new action type to rootReducer

--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,10 +1,16 @@
-import initialState from '../store/initialState'
+import initialState from "../store/initialState";
 const rootReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case "SET_NEWS_FEED":
 			return {
 				...state,
-        newsFeed: action.payload,
+				newsFeed: action.payload,
+			};
+
+		case "SEARCH_NEWS":
+			return {
+				...state,
+				specificNews: action.payload,
 			};
 
 		default:
