b69529e010e179e475f62ccb50e75b35aab77125
Sanne <sannelindholm@hotmail.com>
2020-12-21 23:21:59 +0100

    Addinh set state to onchange event and addinh button from sematic ui with onClick function to set of axios request

--- src/components/NewsSearch.jsx
+++ src/components/NewsSearch.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import NewsService from "../modules/NewsService";
-import NewsCard from "./NewsCard";
-import { Input, Card } from "semantic-ui-react";
+import { Input, Button } from "semantic-ui-react";
 
 const NewsSearch = () => {
 	const dispatch = useDispatch();
-	const [searchQuery, setSearchQuery] = useState()
+	const [searchQuery, setSearchQuery] = useState();
 	const searchNews = async () => {
 		let searchResponse = await NewsService.search(searchQuery);
 		dispatch({ type: "SEARCH_NEWS", payload: searchResponse });
 	};
 
-	useEffect(searchNews, []);
-
 	return (
-		<Input
-			data-cy="search-input"
-			action="Search"
-			placeholder="Search..."
-			onChange={(event) => searchValue(event)}
-		/>
+		<>
+			<Input
+				data-cy="search-input"
+				type="text"
+				placeholder="Search..."
+				onChange={(event) => setSearchQuery(event)}
+			/>
+			<Button data-cy="search-button" onClick={searchNews}>
+				Search
+			</Button>
+		</>
 	);
 };
 
