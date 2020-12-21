5ee3591162c64a027ae18ed6098fddd9a9abf87a
Kyungin Na <nakyungin93@gmail.com>
2020-12-21 22:13:15 +0100

    Styling headers, menu and adding a message for when there is no article to display

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,35 +1,46 @@
 import React from 'react'
 import { useSelector } from "react-redux";
-import { Card, Image, Icon } from 'semantic-ui-react'
+import { Card, Image, Icon, Message } from 'semantic-ui-react'
 
 const NewsCard = () => {
   const articles = useSelector(state => state.newsFeed.articles)
 
   return (
     <>
-      { articles && (
-        <Card.Group itemsPerRow="4" >
-          {articles.map(article => {
-            return (
-              <Card key={article.id} data-cy={`article-${article.id}`} >
-                <Image
-                  src={`${article.image}`}
-                  onError={(e) => { e.target.onerror = null; e.target.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png" }}
-                />
-                <Card.Content>
-                  <Card.Header>{article.title}</Card.Header>
-                  <Card.Meta>{article.published_at}</Card.Meta>
-                  <Card.Description>{article.description}</Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a href={article.url}>
-                    <Icon name='newspaper outline' />Read more</a>
-                </Card.Content>
-              </Card>
-            )
-          })}
-        </Card.Group>
-      )
+      { articles.length > 0 ?
+        <>
+          <Card.Group itemsPerRow="4" >
+            {articles.map(article => {
+              return (
+                <Card
+                  key={article.id}
+                  data-cy={`article-${article.id}`} >
+                  <Image
+                    src={`${article.image}`}
+                    onError={(e) => { e.target.onerror = null; e.target.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png" }}
+                  />
+                  <Card.Content>
+                    <Card.Header>{article.title}</Card.Header>
+                    <Card.Meta>{article.published_at}</Card.Meta>
+                    <Card.Description>{article.description}</Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <a href={article.url}>
+                      <Icon name='newspaper outline' />Read more
+                  </a>
+                  </Card.Content>
+                </Card>
+              )
+            })}
+          </Card.Group>
+        </>
+        :
+        <Message
+          negative
+          size='big'
+          icon='exclamation'
+          header='No article to display'
+        />
       }
     </>
   )
