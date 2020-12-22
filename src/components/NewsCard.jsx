b105ec9cd6fee04118ba2d1678bf0e577b2b8965
sealfury <willspecker@gmail.com>
2020-12-22 22:10:07 +0100

    Beginning to incorporate redux elements into news index

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -3,13 +3,6 @@ import { Header, Card } from 'semantic-ui-react'
 
 const NewsCard = (props) => {
   return (
-    // <Card
-    //   data-cy={`article-${article.id}`}
-    //   header='DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017'
-    //   meta={`By: ${'Lorenzo Stroe'} at ${'FXStreet'}`}
-    //   description='DBS Bank of Singapore has just announced the launch of a digital asset exchange featuring Bitcoin, Ethereum, Bitcoin Cash, and XRP and several fiat cu'
-    //   extra={<Header as='h6'>`Published at ${"2020-12-10T20:29:20Z"}`</Header>}
-    // />
     <Card data-cy={`article-${article.id}`}>
       <Card.Content>
         <Card.Header data-cy="article-title">
--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import NewsCard from "./NewsCard";
 import { Grid } from "semantic-ui-react";
+import NewsService from "../modules/NewsService";
 
 const NewsIndex = () => {
+  const dispatch = useDispatch()
+  const articles = useSelector(state => state.newsFeed)
+  const fetchArticles = async () => {
+    let articles = await NewsService.index()
+    dispatch({ type: "SET_NEWS_FEED", payload: articles.data.data })
+  }
+
+  useEffect(fetchArticles, [])
   return (
     <>
+      
       <Grid>
         <Grid.Row>
           {articles.map((article) => {
