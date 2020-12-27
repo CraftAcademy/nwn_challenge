1706d54cd3f4ac30a8c3832301d6180876e18abb
Rithreaksa <wonderfulreaksa@gmail.com>
2020-12-27 23:38:27 +0100

    Adds API key of news api to fetch news data

--- src/modules/NewsService.js
+++ src/modules/NewsService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const NewsService = {
   async index() {
     const response = await axios.get(
-      "http://newsapi.org/v2/top-headlines?country=us&apiKey=123"
+      "http://newsapi.org/v2/top-headlines?country=us&apiKey=d8ea72a213734633abb2150dcfe09b51"
     );
     return response.data;
   },
