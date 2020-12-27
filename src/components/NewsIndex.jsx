7b7f6cb4bda3b5f6796200df8b44fdeb0547dcea
flis_ann <ann.flismark@gmail.com>
2020-12-27 20:39:21 +0100

    Starting to fetch articles from api

--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import NewsCard from './NewsCard'
-
+import axios from 'axios'
 
 const NewsIndex = () => {
+  const fetchArticles = async () => {
+    let articles = await axios.get('http://newsapi.org/v2/top-headlines?')
+    debugger
+  }
+
+  useEffect(fetchArticles, [])
+  
   return (
     <>
       <NewsCard />
