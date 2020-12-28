1ab4b220b3b40474fcd31ed49cc2db7a91a80993
flis_ann <ann.flismark@gmail.com>
2020-12-28 20:31:37 +0100

    adding method to index and search in NewsService

--- src/modules/NewsService.js
+++ src/modules/NewsService.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 const NewsService = {
   async index(dispatch) {
 let articleList = await axios.get('https://newsapi.org/v2/top-headlines?country=se&apiKey=48fd0dea52e94e779588814f143e8e88')
-dispatch({ type: "SET_NEWS_FEED", paylod: articleList.data.articles })  
+dispatch({ type: "SET_NEWS_FEED", payload: articleList.data.articles })  
 },
 
   async search(query) {
 let articleSearch = await axios.get('https://newsapi.org/v2/everything?country=se&apiKey=48fd0dea52e94e779588814f143e8e88')
-    dispatchEvent({type: })
+    dispatchEvent({type: "SEARCH_ARTICLES", payload: articleSearch.data.articles})
 }
 }
 
