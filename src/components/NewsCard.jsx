eaa9d4ec51a1f0ce14669aae93629d02d09d5089
Rithreaksa <wonderfulreaksa@gmail.com>
2020-12-30 00:13:22 +0100

    Fix styling of the NewsCard

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -11,16 +11,16 @@ const NewsCard = ({
 }) => {
   return (
     <Card data-cy={`article-${id}`}>
-      <Image data-cy="image" src={`${imageUrl}`} />
+      <Image data-cy="image" wrapped ui={false} src={`${imageUrl}`} />
       <Card.Content>
         <Card.Header data-cy="title">{title}</Card.Header>
         <Card.Description data-cy="description">{description}</Card.Description>
         <Card.Meta data-cy="author">{author}</Card.Meta>
       </Card.Content>
       <Card.Content extra>
-        <Card.Meta data-cy="publication-date" as="h6">
+        <p data-cy="publication-date" as="h6">
           {`Published at ${publishedDate}`}
-        </Card.Meta>
+        </p>
       </Card.Content>
     </Card>
   );
