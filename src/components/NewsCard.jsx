a599410158d712c7d7be7b428179ae7795601bc0
flis_ann <ann.flismark@gmail.com>
2020-12-29 14:39:51 +0100

    Formatting the code in NewsCard

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { Header, Card } from 'semantic-ui-react'
+import React from "react";
+import { Header, Card } from "semantic-ui-react";
 
-const NewsCard = ({article}) => {
+const NewsCard = ({ article }) => {
   return (
     <Card
-    data-cy="article-index"
+      data-cy="article-index"
       image={article.urlToImage}
       header={article.title}
-      meta={`By: ${'article.author'} at ${article.source.name}`}
+      meta={`By: ${"article.author"} at ${article.source.name}`}
       description={article.description}
-      extra={<Header as='h6'>`Published at ${"publishedAt"}`</Header>}
-
+      extra={<Header as="h6">`Published at ${"publishedAt"}`</Header>}
     />
-  )
-}
+  );
+};
 
-export default NewsCard
+export default NewsCard;
