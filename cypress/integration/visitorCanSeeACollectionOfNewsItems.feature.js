d208736fc6aca8c0a9384e5dbdf23c1cea5e2336
flis_ann <ann.flismark@gmail.com>
2020-12-29 14:55:26 +0100

    Changing test to reflect structure of the NewsCard and getting the right properties instead of only headers

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -12,7 +12,10 @@ describe("A user can see list of articles", () => {
 
     it("successfully", () => {
       cy.get("[data-cy='article-index']").within(() => {
-        cy.get(".header).should("contain "How Do Bitcoin Transactions Work?");
+        cy.get(".image").should("exist")
+        cy.get(".header").should("contain", "How Do Bitcoin Transactions Work?");
+        cy.get(".meta").should("contain", "modemflame5")
+        cy.get(".description").should("contain", "Moreover Bitcoin works as an investment take benefit cryptocurrency and so forth also. But that changed on their Bitcoin HYIP the corporation has modified the investment. Engineers of Bitcoin SV designers can seize info regarding mining OS that. Mining the cu…")
         
       });
     });
