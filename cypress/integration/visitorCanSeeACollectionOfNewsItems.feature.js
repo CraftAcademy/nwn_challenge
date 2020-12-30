89587183260afd4bbf8d5c06eb48bca4f0dbde5b
flis_ann <ann.flismark@gmail.com>
2020-12-30 15:53:55 +0100

    Adding test for if there are no articles

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -12,11 +12,33 @@ describe("A user can see list of articles", () => {
 
     it("successfully", () => {
       cy.get("[data-cy='article-index']").within(() => {
-        cy.get(".image").should("exist")
-        cy.get(".header").should("contain", "How Do Bitcoin Transactions Work?");
-        cy.get(".meta").should("contain", "modemflame5")
-        cy.get(".description").should("contain", "Moreover Bitcoin works as an investment take benefit cryptocurrency and so forth also. But that changed on their Bitcoin HYIP the corporation has modified the investment. Engineers of Bitcoin SV designers can seize info regarding mining OS that. Mining the cu…")
-        
+        cy.get(".image").should("exist");
+        cy.get(".header").should(
+          "contain",
+          "How Do Bitcoin Transactions Work?"
+        );
+        cy.get(".meta").should("contain", "modemflame5");
+        cy.get(".description").should(
+          "contain",
+          "Moreover Bitcoin works as an investment take benefit cryptocurrency and so forth also. But that changed on their Bitcoin HYIP the corporation has modified the investment. Engineers of Bitcoin SV designers can seize info regarding mining OS that. Mining the cu…"
+        );
+      });
+    });
+
+    describe("if there are no articles", () => {
+      before(() => {
+        cy.server();
+        cy.route({
+          method: "GET",
+          url: "https://newsapi.org/v2/top-headlines?country=se&apiKey=**",
+          response: { articles: [] },
+        });
+        cy.visit("/");
+      });
+
+      it("is presented with an error message", () => {
+        cy.get("[data-cy='article-index']").should("not.exist");
+        cy.get("[data-cy='empty-index']").should("exist");
       });
     });
   });
