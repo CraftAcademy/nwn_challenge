2e2fba1ea70b22f38330f71ca5a41f153de5d52e
flis_ann <ann.flismark@gmail.com>
2020-12-30 16:55:54 +0100

    Adding functionality to input field; connecting setter. Connecting function to button

--- src/components/NewsSearch.jsx
+++ src/components/NewsSearch.jsx
@@ -1,29 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import NewsService from "../modules/NewsService";
 import { Button, Input, List } from "semantic-ui-react";
 
 function NewsSearch() {
   const dispatch = useDispatch();
-  const [searchResult, setSearchResult] = useState()
+  const [searchResult, setSearchResult] = useState();
   const fetchArticleSearch = async () => {
-    let articlesResult = await NewsService.search(searchResult)
-    dispatch({ type: "SET_NEWS_FEED", payload: articlesResult })
-  }
-
-    return (
-      <>
-        <Input type="text" data-cy="search_input" />
-        <Button type="submit" data-cy="search_button">
-          Search
-        </Button>
-        <div data-cy="search_results">
-          <List>
-            <List.Item>{showArticles}</List.Item>
-          </List>
-        </div>
-      </>
-    );
+    let articlesResult = await NewsService.search(searchResult);
+    dispatch({ type: "SET_NEWS_FEED", payload: articlesResult });
   };
+
+  return (
+    <>
+      <Input
+        data-cy="search_input"
+        type="text"
+        placeholder="Search..."
+        onBlur={(event) => setSearchResult(event.target.value)}
+      />
+      <Button  
+      data-cy="search_button"
+      onClick={fetchArticleSearch}
+      >
+        Search
+      </Button>
+      
+    </>
+  );
+}
 
 export default NewsSearch;
