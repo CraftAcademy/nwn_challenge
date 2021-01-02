f17a92aec33d798007e836c3aa7a13a94fc03bbb
Ehugo2000 <erik.hgsn@gmail.com>
2021-01-02 14:46:44 +0100

    Writing first test to find newscard.

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,6 +1,24 @@
 /* eslint-disable no-undef */
-describe('', () => {
-  it('', () => {
-    cy.visit('/')
+describe("On index page ", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://newsapi.org/v2/top-headlines",
+      response: "fixture:new_index.json",
+    });
+    cy.visit("/");
+  });
+
+  it("user can see first article card ", () => {
+    cy.get("[data-cy='news-card']").within(() => {
+      cy.contains(
+        "DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017"
+      );
+      cy.contains("FXStreet");
+      cy.contains(
+        "DBS Bank of Singapore has just announced the launch of a digital asset exchange featuring Bitcoin"
+      );
+    });
   });
 });
