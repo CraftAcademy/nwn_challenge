d592f46baec06b5eee0c5c9c0617ced345213f06
Ehugo2000 <erik.hgsn@gmail.com>
2021-01-02 19:05:24 +0100

    Find how tho fetch News API.

--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import NewsCard from './NewsCard'
+import axios from 'axios'
+import NewsService from '../modules/NewsService'
 
 
 const NewsIndex = () => {
+  const [articlesData, setArticlesData] = useState([])
+  const getNewsService = async () => {
+    let response = await NewsService.index()
+    setArticlesData(response)
+    console.log(response)
+  }
+  
+  useEffect(() => {
+    getNewsService();
+  }, []);
+
   return (
     <>
       <NewsCard />
+      <div>
+        {articlesData.map(article => {
+          return (<li key={article.id}>{article.title}</li>)
+        })}
+      </div>
     </>
   )
 }
--- src/modules/NewsService.js
+++ src/modules/NewsService.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 const NewsService = {
   async index() {
+    let result = await axios.get('http://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=dae7714c2de74ce1b1c383440f50a9e9')
+    return result.data.articles
 
   },
 
