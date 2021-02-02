9a6378c4e42fa7296f0ccafa2d8178d160aa931b
Thomas <thomas@craftacademy.se>
2021-02-02 15:45:46 +0100

    WIP adds functionality to dissplay headline news styling is off atm

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,5 +1,6 @@
-describe('', () => {
-  it('', () => {
+describe('Visitor can see a set of Headline news', () => {
+  it('when visiting the application', () => {
     cy.visit('/')
+    cy.get("[data-cy='news-display']").children().should('have.length', 20)
   });
 });
